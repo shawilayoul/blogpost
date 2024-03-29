0d026ea2f02b00ed57491085fec4b11639fd1b89
shawil ayoul <aocholayoul9@gmail.com>
2024-03-29 10:35:21 +0100

    displaying data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      home page
-    </div>
-  )
-}
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((resp) => resp.json().then((data) => setPosts(data)))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(posts);
+  return <div>home page</div>;
+};
 
-export default HomePage
+export default HomePage;
